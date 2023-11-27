9419cf55ae365880a0e1d390a1f74e15160e65f7
shindaeho <sdh077@naver.com>
2023-11-27 21:20:12 +0900

    detail

---
+++ src/app/sermon/[id]/page.tsx
@@ -0,0 +1,92 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Border } from '@/components/Border'
+import { Button } from '@/components/Button'
+import { ContactSection } from '@/components/ContactSection'
+import { Container } from '@/components/Container'
+import { FadeIn } from '@/components/FadeIn'
+import { PageIntro } from '@/components/PageIntro'
+import { formatDate } from '@/lib/formatDate'
+import { supabase } from '@/lib/api'
+
+export const metadata: Metadata = {
+    title: 'Blog',
+    description:
+        'Stay up-to-date with the latest industry news as our marketing teams finds new ways to re-purpose old CSS tricks articles.',
+}
+const getData = async () => {
+    return await supabase.from('archive').select()
+}
+export default async function BlogDetail() {
+    const { data, error } = await getData()
+
+    return (
+        <>
+            <PageIntro eyebrow="Blog" title="The latest articles and news">
+                <p>
+                    Stay up-to-date with the latest industry news as our marketing teams
+                    finds new ways to re-purpose old CSS tricks articles.
+                </p>
+            </PageIntro>
+
+            <Container className="mt-24 sm:mt-32 lg:mt-40">
+                <div className="space-y-24 lg:space-y-32">
+                    {data && data.map((article) => (
+                        <FadeIn key={article.id}>
+                            <article>
+                                <Border className="pt-16">
+                                    <div className="relative lg:-mx-4 lg:flex lg:justify-end">
+                                        <div className="pt-10 lg:w-2/3 lg:flex-none lg:px-4 lg:pt-0">
+                                            <h2 className="font-display text-2xl font-semibold text-neutral-950">
+                                                <Link href={`/sermon/${article.id}`}>{article.title}</Link>
+                                            </h2>
+                                            <dl className="lg:absolute lg:left-0 lg:top-0 lg:w-1/3 lg:px-4">
+                                                <dt className="sr-only">Published</dt>
+                                                <dd className="absolute left-0 top-0 text-sm text-neutral-950 lg:static">
+                                                    <time dateTime={article.date}>
+                                                        {article.play_dt ? formatDate(article.play_dt) : ''}
+                                                    </time>
+                                                </dd>
+                                                <dt className="sr-only">Author</dt>
+                                                <dd className="mt-6 flex gap-x-4">
+                                                    <div className="overflow-hidden rounded-xl bg-neutral-100">
+                                                        <img
+                                                            alt=""
+                                                            src={`https://i.ytimg.com/vi/${article.link}/hqdefault.jpg`}
+                                                            className="object-cover grayscale sm:w-full lg:w-64"
+                                                        />
+                                                    </div>
+                                                </dd>
+                                            </dl>
+                                            <p className="mt-6 max-w-2xl text-base text-neutral-600">
+                                                {article.description}
+                                            </p>
+                                            <Button
+                                                href={`/sermon/${article.id}`}
+                                                aria-label={`Read more: ${article.title}`}
+                                                className="mt-8 mr-2"
+                                            >
+                                                자세히 보기
+                                            </Button>
+                                            <Button
+                                                href={`https://www.youtube.com/watch?v=${article.link}`}
+                                                aria-label={`Read more: ${article.title}`}
+                                                className="mt-8"
+                                            >
+                                                youtube 방문
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </Border>
+                            </article>
+                        </FadeIn>
+                    ))}
+                </div>
+            </Container>
+
+            <ContactSection />
+        </>
+    )
+}
