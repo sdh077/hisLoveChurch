83d4f5ecd9aa7a4d9752b72dfefcd7fb8451175c
raymond <leedg84@gmail.com>
2024-02-05 11:08:31 +0900

    naver meta tag 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+      <head>
+        <meta name="naver-site-verification" content="99063f5c54e645cca7de4ae64d1e1916c342b446" />
+
+      </head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
